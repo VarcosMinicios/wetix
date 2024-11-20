4da1a00f37a544bb78753a753ae8bbf026013214
VarcosMinicios <marcos11vini2@outlook.com>
2024-11-20 19:35:21 -0300

    try catch

--- app/Http/Controllers/FrontEnd/PaymentGateway/AsaasController.php
+++ app/Http/Controllers/FrontEnd/PaymentGateway/AsaasController.php
@@ -168,57 +168,62 @@ class AsaasController extends Controller
 
   public function handleAsaas(Request $request)
   {
-    $payload = $request->all();
-    $event = $payload['event'];
+    try {
 
-    if ($event === 'PAYMENT_CONFIRMED') {
-      if ($payload['payment']['status'] === 'CONFIRMED') {
-        $checkoutData = CheckoutData::where('payment_id', $payload['payment']['id'])->first();
-        $checkoutData->status = 'completed';
+      $payload = $request->all();
+      $event = $payload['event'];
 
-        $bookingController = new BookingController();
-        $bookingInfo = $bookingController->getBookingInfo($checkoutData->id);
+      if ($event === 'PAYMENT_CONFIRMED') {
+        if ($payload['payment']['status'] === 'CONFIRMED') {
+          $checkoutData = CheckoutData::where('payment_id', $payload['payment']['id'])->first();
+          $checkoutData->status = 'completed';
 
-        $invoice = $bookingController->generateInvoice($bookingInfo, $checkoutData->event_id);
-        //unlink qr code
-        @unlink(public_path('assets/admin/qrcodes/') . $bookingInfo->booking_id . '.svg');
-        //end unlink qr code
+          $bookingController = new BookingController();
+          $bookingInfo = $bookingController->getBookingInfo($checkoutData->id);
 
-        // then, update the invoice field info in database
-        $bookingInfo->update(['invoice' => $invoice]);
+          $invoice = $bookingController->generateInvoice($bookingInfo, $checkoutData->event_id);
+          //unlink qr code
+          @unlink(public_path('assets/admin/qrcodes/') . $bookingInfo->booking_id . '.svg');
+          //end unlink qr code
 
-        //add blance to admin revinue
-        $earning = Earning::first();
-        $earning->total_revenue = $earning->total_revenue + $checkoutData->price + $bookingInfo->tax;
-        if ($bookingInfo['organizer_id'] != null) {
-          $earning->total_earning = $earning->total_earning + ($bookingInfo->tax + $bookingInfo->commission);
-        } else {
-          $earning->total_earning = $earning->total_earning + $checkoutData->price + $bookingInfo->tax;
+          // then, update the invoice field info in database
+          $bookingInfo->update(['invoice' => $invoice]);
+
+          //add blance to admin revinue
+          $earning = Earning::first();
+          $earning->total_revenue = $earning->total_revenue + $checkoutData->price + $bookingInfo->tax;
+          if ($bookingInfo['organizer_id'] != null) {
+            $earning->total_earning = $earning->total_earning + ($bookingInfo->tax + $bookingInfo->commission);
+          } else {
+            $earning->total_earning = $earning->total_earning + $checkoutData->price + $bookingInfo->tax;
+          }
+          $earning->save();
+
+          //storeTransaction
+          $bookingInfo['paymentStatus'] = 1;
+          $bookingInfo['transcation_type'] = 1;
+
+          storeTranscation($bookingInfo);
+
+          //store amount to organizer
+          $organizerData['organizer_id'] = $bookingInfo['organizer_id'];
+          $organizerData['price'] = $checkoutData->price;
+          $organizerData['tax'] = $bookingInfo->tax;
+          $organizerData['commission'] = $bookingInfo->commission;
+          storeOrganizer($organizerData);
+
+          // send a mail to the customer with the invoice
+          $bookingController->sendMail($bookingInfo);
+
+          return redirect()->route('event_booking.complete', [
+            'id' => $checkoutData->event_id, 'booking_id' => $bookingInfo->id
+          ]);
         }
-        $earning->save();
-
-        //storeTransaction
-        $bookingInfo['paymentStatus'] = 1;
-        $bookingInfo['transcation_type'] = 1;
-
-        storeTranscation($bookingInfo);
-
-        //store amount to organizer
-        $organizerData['organizer_id'] = $bookingInfo['organizer_id'];
-        $organizerData['price'] = $checkoutData->price;
-        $organizerData['tax'] = $bookingInfo->tax;
-        $organizerData['commission'] = $bookingInfo->commission;
-        storeOrganizer($organizerData);
-
-        // send a mail to the customer with the invoice
-        $bookingController->sendMail($bookingInfo);
-
-        return redirect()->route('event_booking.complete', [
-          'id' => $checkoutData->event_id, 'booking_id' => $bookingInfo->id
-        ]);
       }
-    }
 
-    return response()->json(['message' => 'Webhook received']);
+      return response()->json(['message' => 'Webhook received']);
+    } catch (\Exception $e) {
+      return response()->json(['message' => $e->getMessage()]);
+    }
   }
 }
